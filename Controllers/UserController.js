e806f5d44c3c4bb303bae753ca1dac603ca0672a
yuragms <yura.gms@gmail.com>
2022-08-09 22:05:12 +0300

    user_id

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -6,7 +6,14 @@ export const getUser = async (req, res) => {
 
   try {
     const user = await UserModel.findById(id);
-    res.status(200).json(user);
+    // res.status(200).json(user);
+    if (user) {
+      const { password, ...otherDetails } = user._doc;
+
+      res.status(200).json(otherDetails);
+    } else {
+      res.status(404).json("No such user exists");
+    }
   } catch (error) {
     res.status(500).json(error);
   }
